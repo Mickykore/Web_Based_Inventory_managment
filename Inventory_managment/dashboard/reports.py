4465ded1b4807fc3d69d823ba561a0100d4f3682
mickykore <mikiyasayele6@gmail.com>
2023-06-01 11:47:04 +0300

    add CRUD functionality

--- Inventory_managment/dashboard/reports.py
+++ Inventory_managment/dashboard/reports.py
@@ -4,28 +4,29 @@ from django.utils import timezone
 from datetime import timedelta
 
 def get_daily_sales():
-    today = timezone.now().date()
+    today = timezone.localtime().date()  # Convert to the local timezone
     daily_sales = Sales.objects.filter(sale_date__date=today)
     total_sales = daily_sales.aggregate(total_sales=Sum('sale_price'))['total_sales']
     return daily_sales, total_sales
 
 def get_weekly_sales():
-    start_of_week = timezone.now().date() - timedelta(days=timezone.now().date().weekday())
+    start_of_week = timezone.localtime().date() - timedelta(days=timezone.localtime().date().weekday())
     end_of_week = start_of_week + timedelta(days=6)
     weekly_sales = Sales.objects.filter(sale_date__date__range=[start_of_week, end_of_week])
     total_sales = weekly_sales.aggregate(total_sales=Sum('sale_price'))['total_sales']
     return weekly_sales, total_sales
 
-
 def get_monthly_sales():
-    current_month = timezone.now().date().month
+    current_month = timezone.localtime().date().month
     monthly_sales = Sales.objects.filter(sale_date__month=current_month)
     total_sales = monthly_sales.aggregate(total_sales=Sum('sale_price'))['total_sales']
     return monthly_sales, total_sales
 
 def get_yearly_sales():
-    current_year = timezone.now().date().year
+    current_year = timezone.localtime().date().year
     yearly_sales = Sales.objects.filter(sale_date__year=current_year)
     total_sales = yearly_sales.aggregate(total_sales=Sum('sale_price'))['total_sales']
     return yearly_sales, total_sales
+
+
 
--- Inventory_managment/dashboard/views.py
+++ Inventory_managment/dashboard/views.py
@@ -2,50 +2,67 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .reports import *
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from .form import ProductForm
 @login_required(login_url='/login/')
 def home(request):
     return render(request, 'dashboard/home.html')
     # return HttpResponse('<h1>Home page</h1>')
+    
+    
 @login_required(login_url='/login/')
 def product(request):
-    return render(request, 'dashboard/product.html')
+    products = Product.objects.all()
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+    else:
+        form = ProductForm()
+    context = {
+        'products': products,
+        'form': form,
+    }
+    return render(request, 'dashboard/product.html', context)
+
 
 @login_required(login_url='/login/')
 def sales(request):
     return render(request, 'dashboard/sales.html')
 
-@login_required(login_url='/login/')
-def report(request):
-    return render(request, 'dashboard/report.html')
+# @login_required(login_url='/login/')
+# def report(request):
+#     return render(request, 'dashboard/report.html')
+
+# @login_required(login_url='/login/')
+# def reports(request):
+#     daily_sales, total_sales = get_daily_sales()  # Get daily sales report
+
+#     daily_sales_data = []
+#     total_profit = 0
+
+#     for sale in daily_sales:
+#         sale_data = {
+#             'product_name': sale.product_name.name,
+#             'product_category': sale.product_name.category.category,
+#             'purchased_price': sale.product_name.price,
+#             'sale_price': sale.sale_price,
+#             'quantity': sale.sale_quantity,
+#         }
+#         sale_data['profit'] = sale_data['sale_price'] - sale_data['purchased_price']
+#         sale_data['total_sale'] = sale_data['sale_price'] * sale_data['quantity']
+#         sale_data['total_profit'] = sale_data['profit'] * sale_data['quantity']
+#         daily_sales_data.append(sale_data)
+
+#         total_profit += sale_data['total_profit']
+
+#     context = {
+#         'daily_sales': daily_sales_data,
+#         'total_sales': total_sales,
+#         'total_profit': total_profit,
+#     }
+#     return render(request, 'dashboard/report.html', context)
 
 @login_required(login_url='/login/')
-def reports(request):
-    daily_sales, total_sales = get_daily_sales()  # Get daily sales report
-
-    daily_sales_data = []
-    total_profit = 0
-
-    for sale in daily_sales:
-        sale_data = {
-            'product_name': sale.product_name.name,
-            'product_category': sale.product_name.category.category,
-            'purchased_price': sale.product_name.price,
-            'sale_price': sale.sale_price,
-            'quantity': sale.sale_quantity,
-        }
-        sale_data['profit'] = sale_data['sale_price'] - sale_data['purchased_price']
-        sale_data['total_sale'] = sale_data['sale_price'] * sale_data['quantity']
-        sale_data['total_profit'] = sale_data['profit'] * sale_data['quantity']
-        daily_sales_data.append(sale_data)
-
-        total_profit += sale_data['total_profit']
-
-    context = {
-        'daily_sales': daily_sales_data,
-        'total_sales': total_sales,
-        'total_profit': total_profit,
-    }
-    return render(request, 'dashboard/report.html', context)
 def reports(request):
     daily_sales, total_daily_sales = get_daily_sales()  # Get daily sales report
     weekly_sales, total_weekly_sales = get_weekly_sales()  # Get weekly sales report
@@ -54,6 +71,7 @@ def reports(request):
 
     daily_sales_data = []
     total_profit = 0
+    total_sale_amount = 0  # Add a variable to keep track of the total sales amount
 
     for sale in daily_sales:
         sale_data = {
@@ -69,6 +87,7 @@ def reports(request):
         daily_sales_data.append(sale_data)
 
         total_profit += sale_data['total_profit']
+        total_sale_amount += sale_data['total_sale']  # Increment the total sales amount
 
     context = {
         'daily_sales': daily_sales_data,
@@ -80,5 +99,6 @@ def reports(request):
         'yearly_sales': yearly_sales,
         'total_yearly_sales': total_yearly_sales,
         'total_profit': total_profit,
+        'total_sale_amount': total_sale_amount,  # Add the total sales amount to the context
     }
     return render(request, 'dashboard/report.html', context)
