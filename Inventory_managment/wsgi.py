6fdee5edde321a2f48b08de694db606aeac5b049
Mikiyas Ayele Kore <111088696+Mickykore@users.noreply.github.com>
2024-04-02 16:27:14 +0300

    Update wsgi.py

--- Inventory_managment/wsgi.py
+++ Inventory_managment/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Inventory_managment.settings')
 
 application = get_wsgi_application()
+
+app = application
